4117721b3e9ea9fe32cc29c48ab9378000e75c3b
Chen Minqiang <ptpt52@gmail.com>
2020-04-09 08:51:44 +0800

    NATFLOW_FASTNAT_TABLE_SIZE 4096 on large memory machine

--- natflow.h
+++ natflow.h
@@ -134,7 +134,11 @@ struct natflow_fastnat_node_t {
 #define NATFLOW_FF_TIMEOUT_LOW (25 * HZ)
 
 /* MAX 65536 for now we use 2048 */
+#if defined(CONFIG_64BIT) || defined(CONFIG_X86) || defined(CONFIG_X86_64) || defined(CONFIG_ARM) || defined(CONFIG_ARM64)
+#define NATFLOW_FASTNAT_TABLE_SIZE 4096
+#else
 #define NATFLOW_FASTNAT_TABLE_SIZE 2048
+#endif
 
 static inline u32 natflow_hash_v4(__be32 saddr, __be32 daddr, __be16 source, __be16 dest, __be16 proto)
 {
