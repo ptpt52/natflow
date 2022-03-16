8a6bf377f2aa48d1f2fe747daff5a21e24464908
Chen Minqiang <ptpt52@gmail.com>
2022-03-16 12:47:43 +0800

    path: skip nfn stats if sample up to slow path

--- natflow_path.c
+++ natflow_path.c
@@ -565,6 +565,19 @@ static unsigned int natflow_path_pre_ct_in_hook(void *priv,
 					goto out;
 				}
 
+				/* sample up to slow path every 5s */
+				if ((u32)ucharmindiff(((nfn->jiffies / HZ) & 0xff), nfn->count) >= NATFLOW_FF_SAMPLE_TIME && !test_and_set_bit(0, &nfn->status)) {
+					unsigned long bytes = nfn->flow_bytes;
+					unsigned long packets = nfn->flow_packets;
+					nfn->count = (nfn->jiffies / HZ) & 0xff;
+					natflow_offload_keepalive(hash, bytes, packets, nfn->speed_bytes, nfn->speed_packets);
+					nfn->flow_bytes -= bytes;
+					nfn->flow_packets -= packets;
+					wmb();
+					clear_bit(0, &nfn->status);
+					goto out;
+				}
+
 				do {
 					_I = (jiffies / HZ) % 4;
 					nfn->speed_bytes[_I] += skb->len;
@@ -578,19 +591,6 @@ static unsigned int natflow_path_pre_ct_in_hook(void *priv,
 					}
 					nfn->speed_jiffies = jiffies;
 				} while (0);
-
-				/* sample up to slow path every 5s */
-				if ((u32)ucharmindiff(((nfn->jiffies / HZ) & 0xff), nfn->count) >= NATFLOW_FF_SAMPLE_TIME && !test_and_set_bit(0, &nfn->status)) {
-					unsigned long bytes = nfn->flow_bytes;
-					unsigned long packets = nfn->flow_packets;
-					nfn->count = (nfn->jiffies / HZ) & 0xff;
-					natflow_offload_keepalive(hash, bytes, packets, nfn->speed_bytes, nfn->speed_packets);
-					nfn->flow_bytes -= bytes;
-					nfn->flow_packets -= packets;
-					wmb();
-					clear_bit(0, &nfn->status);
-					goto out;
-				}
 
 #if (defined(CONFIG_NET_RALINK_OFFLOAD) || defined(NATFLOW_OFFLOAD_HWNAT_FAKE) && defined(CONFIG_NET_MEDIATEK_SOC))
 				if ((nfn->flags & FASTNAT_EXT_HWNAT_FLAG)) {
