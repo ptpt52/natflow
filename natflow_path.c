a097276b6972b3a851612d281254285837206d2c
Chen Minqiang <ptpt52@gmail.com>
2022-03-27 19:50:22 +0800

    hwnat: fix extdev stats flow speed
    for some reason pkts from extdev to ppe no keepalive trigger

--- natflow_path.c
+++ natflow_path.c
@@ -591,6 +591,32 @@ static unsigned int natflow_path_pre_ct_in_hook(void *priv,
 					goto out;
 				}
 
+				/* sample up to slow path every 5s */
+				if ((u32)ucharmindiff(((nfn->jiffies / HZ) & 0xff), nfn->count) >= NATFLOW_FF_SAMPLE_TIME && !test_and_set_bit(0, &nfn->status)) {
+					unsigned long bytes = nfn->flow_bytes;
+					unsigned long packets = nfn->flow_packets;
+					natflow_offload_keepalive(hash, bytes, packets, nfn->speed_bytes, nfn->speed_packets);
+					nfn->flow_bytes -= bytes;
+					nfn->flow_packets -= packets;
+					nfn->count = (nfn->jiffies / HZ) & 0xff;
+					wmb();
+					clear_bit(0, &nfn->status);
+					goto out;
+				}
+				do {
+					_I = (jiffies / HZ) % 4;
+					nfn->speed_bytes[_I] += skb->len;
+					nfn->speed_packets[_I] += 1;
+					nfn->flow_bytes += skb->len;
+					nfn->flow_packets += 1;
+					_I = (_I + 1) % 4;
+					if (nfn->speed_bytes[_I] != 0) {
+						nfn->speed_bytes[_I] = 0;
+						nfn->speed_packets[_I] = 0;
+					}
+					nfn->speed_jiffies = jiffies;
+				} while (0);
+
 #if (defined(CONFIG_NET_RALINK_OFFLOAD) || defined(NATFLOW_OFFLOAD_HWNAT_FAKE) && defined(CONFIG_NET_MEDIATEK_SOC))
 				if ((nfn->flags & FASTNAT_EXT_HWNAT_FLAG)) {
 #if defined(CONFIG_HWNAT_EXTDEV_USE_VLAN_HASH) && !defined(CONFIG_HWNAT_EXTDEV_DISABLED)
@@ -622,31 +648,6 @@ static unsigned int natflow_path_pre_ct_in_hook(void *priv,
 					return NF_STOLEN;
 				}
 #endif
-				/* sample up to slow path every 5s */
-				if ((u32)ucharmindiff(((nfn->jiffies / HZ) & 0xff), nfn->count) >= NATFLOW_FF_SAMPLE_TIME && !test_and_set_bit(0, &nfn->status)) {
-					unsigned long bytes = nfn->flow_bytes;
-					unsigned long packets = nfn->flow_packets;
-					natflow_offload_keepalive(hash, bytes, packets, nfn->speed_bytes, nfn->speed_packets);
-					nfn->flow_bytes -= bytes;
-					nfn->flow_packets -= packets;
-					nfn->count = (nfn->jiffies / HZ) & 0xff;
-					wmb();
-					clear_bit(0, &nfn->status);
-					goto out;
-				}
-				do {
-					_I = (jiffies / HZ) % 4;
-					nfn->speed_bytes[_I] += skb->len;
-					nfn->speed_packets[_I] += 1;
-					nfn->flow_bytes += skb->len;
-					nfn->flow_packets += 1;
-					_I = (_I + 1) % 4;
-					if (nfn->speed_bytes[_I] != 0) {
-						nfn->speed_bytes[_I] = 0;
-						nfn->speed_packets[_I] = 0;
-					}
-					nfn->speed_jiffies = jiffies;
-				} while (0);
 
 				if (!(nfn->flags & FASTNAT_BRIDGE_FWD)) {
 					if (iph->ttl <= 1) {
@@ -778,6 +779,32 @@ fast_output:
 					goto out;
 				}
 
+				/* sample up to slow path every 5s */
+				if ((u32)ucharmindiff(((nfn->jiffies / HZ) & 0xff), nfn->count) >= NATFLOW_FF_SAMPLE_TIME && !test_and_set_bit(0, &nfn->status)) {
+					unsigned long bytes = nfn->flow_bytes;
+					unsigned long packets = nfn->flow_packets;
+					natflow_offload_keepalive(hash, bytes, packets, nfn->speed_bytes, nfn->speed_packets);
+					nfn->flow_bytes -= bytes;
+					nfn->flow_packets -= packets;
+					nfn->count = (nfn->jiffies / HZ) & 0xff;
+					wmb();
+					clear_bit(0, &nfn->status);
+					goto out;
+				}
+				do {
+					_I = (jiffies / HZ) % 4;
+					nfn->speed_bytes[_I] += skb->len;
+					nfn->speed_packets[_I] += 1;
+					nfn->flow_bytes += skb->len;
+					nfn->flow_packets += 1;
+					_I = (_I + 1) % 4;
+					if (nfn->speed_bytes[_I] != 0) {
+						nfn->speed_bytes[_I] = 0;
+						nfn->speed_packets[_I] = 0;
+					}
+					nfn->speed_jiffies = jiffies;
+				} while (0);
+
 #if (defined(CONFIG_NET_RALINK_OFFLOAD) || defined(NATFLOW_OFFLOAD_HWNAT_FAKE) && defined(CONFIG_NET_MEDIATEK_SOC))
 				if ((nfn->flags & FASTNAT_EXT_HWNAT_FLAG)) {
 #if defined(CONFIG_HWNAT_EXTDEV_USE_VLAN_HASH) && !defined(CONFIG_HWNAT_EXTDEV_DISABLED)
@@ -809,31 +836,6 @@ fast_output:
 					return NF_STOLEN;
 				}
 #endif
-				/* sample up to slow path every 5s */
-				if ((u32)ucharmindiff(((nfn->jiffies / HZ) & 0xff), nfn->count) >= NATFLOW_FF_SAMPLE_TIME && !test_and_set_bit(0, &nfn->status)) {
-					unsigned long bytes = nfn->flow_bytes;
-					unsigned long packets = nfn->flow_packets;
-					natflow_offload_keepalive(hash, bytes, packets, nfn->speed_bytes, nfn->speed_packets);
-					nfn->flow_bytes -= bytes;
-					nfn->flow_packets -= packets;
-					nfn->count = (nfn->jiffies / HZ) & 0xff;
-					wmb();
-					clear_bit(0, &nfn->status);
-					goto out;
-				}
-				do {
-					_I = (jiffies / HZ) % 4;
-					nfn->speed_bytes[_I] += skb->len;
-					nfn->speed_packets[_I] += 1;
-					nfn->flow_bytes += skb->len;
-					nfn->flow_packets += 1;
-					_I = (_I + 1) % 4;
-					if (nfn->speed_bytes[_I] != 0) {
-						nfn->speed_bytes[_I] = 0;
-						nfn->speed_packets[_I] = 0;
-					}
-					nfn->speed_jiffies = jiffies;
-				} while (0);
 
 				if (!(nfn->flags & FASTNAT_BRIDGE_FWD)) {
 					if (iph->ttl <= 1) {
