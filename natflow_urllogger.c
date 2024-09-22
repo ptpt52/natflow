5bf72675c0bb21782b13c09f489af58a9a51fccf
Chen Minqiang <ptpt52@gmail.com>
2024-09-22 20:13:06 +0800

    fix sysctl table check failed
    [484924.324330] sysctl table check failed: urllogger_store/(null) procname is null
    [484924.324333] sysctl table check failed: urllogger_store/(null) No proc_handler

--- natflow_urllogger.c
+++ natflow_urllogger.c
@@ -1705,7 +1705,6 @@ static struct ctl_table urllogger_table[] = {
 		.mode           = S_IRUGO|S_IWUSR,
 		.proc_handler   = proc_douintvec,
 	},
-	{ }
 };
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(6, 5, 0)
