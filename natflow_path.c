119b818614e81781bd437deafab873fe501ecfba
Chen Minqiang <ptpt52@gmail.com>
2024-09-26 08:00:18 +0800

    natflow: vline: handle and allow pppoe forwarding

--- natflow_path.c
+++ natflow_path.c
@@ -1469,13 +1469,7 @@ static unsigned int natflow_path_pre_ct_in_hook(void *priv,
 				goto __hook_ipv6_main;
 			}
 
-			if (unlikely(nf_ct_get(skb, &ctinfo) != NULL)) {
-				goto out6;
-			}
-
-			if (skb->pkt_type == PACKET_BROADCAST || skb->pkt_type == PACKET_MULTICAST) {
-				goto out6;
-			}
+			goto out6;
 		}
 
 		if (ingress_pad_len > 0) {
@@ -4714,7 +4708,9 @@ out6:
 
 		//XXXXXXXX
 		if (skb->dev->ifindex < VLINE_FWD_MAX_NUM && vline_fwd_map[skb->dev->ifindex] != NULL) {
-			if (!(skb->dev->flags & IFF_VLINE_FAMILY_IPV4)) {
+			if (!(skb->dev->flags & IFF_VLINE_FAMILY_IPV4) ||
+			        skb->protocol == __constant_htons(ETH_P_PPP_DISC) ||
+			        skb->protocol == __constant_htons(ETH_P_PPP_SES)) {
 				if (skb->pkt_type == PACKET_BROADCAST || skb->pkt_type == PACKET_MULTICAST) {
 					skb = skb_clone(skb, GFP_ATOMIC);
 					if (!skb) {
