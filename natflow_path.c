874c41f26a54920ef7577cf4fe6be8b842be2821
Chen Minqiang <ptpt52@gmail.com>
2026-05-16 11:17:04 +0800

    natflow: fix GRO disabling logic in netdev notifier
    Signed-off-by: Chen Minqiang <ptpt52@gmail.com>

--- natflow_path.c
+++ natflow_path.c
@@ -5941,9 +5941,9 @@ static int natflow_netdev_event(struct notifier_block *this, unsigned long event
 #ifdef CONFIG_NETFILTER_INGRESS
 	if (event == NETDEV_UP) {
 		if ((dev->features & (NETIF_F_GRO | NETIF_F_GRO_FRAGLIST))) {
-			dev->features &= ~(NETIF_F_GRO | NETIF_F_GRO_FRAGLIST);
+			dev->wanted_features &= ~(NETIF_F_GRO | NETIF_F_GRO_FRAGLIST);
 			netdev_update_features(dev);
-			NATFLOW_println("remove NETIF_F_GRO for dev=%s\n", dev->name);
+			NATFLOW_println("remove NETIF_F_GRO/NETIF_F_GRO_FRAGLIST for dev=%s\n", dev->name);
 		}
 		if (!((dev->flags & IFF_LOOPBACK) ||
 		        netif_is_bridge_master(dev) ||
